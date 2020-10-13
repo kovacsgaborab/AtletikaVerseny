85de82669e1c513ebb0c8c3c6b7b520885f231ac
kovacsgaborab <kovacsgaborab@gmail.com>
2020-10-13 09:56:35 +0200

    Megoldva

---
+++ AtletikaiVerseny/Atleta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtletikaiVerseny
+{
+    class Atleta
+    {
+        public string Rajtszam { get; private set; }
+        public string VezNev { get; private set; }
+        public string KerNev { get; private set; }
+        public string Egyesulet { get; private set; }
+        public int Ugras { get; private set; }
+
+
+        public Atleta(string sor)
+        {
+            string Nev;
+
+            
+
+            string[] a = sor.Split(';');
+            Rajtszam = a[0];
+
+            
+            Nev = a[1];
+
+            string[] b = Nev.Split(' ');
+            VezNev = b[0];
+            KerNev = b[1];
+
+            Egyesulet = a[2];
+            Ugras =Convert.ToInt32(a[3]);
+        }
+
+
+        //public string Nev
+        //{
+        //    string nev = "";
+
+        //    string[] a = a.Split(' ');
+        //    KerNev = a[3];
+        //    VezNev = a[4];
+
+        //    nev = KerNev + VezNev;
+
+        //    return nev;
+        //}
+
+    }
+}
---
+++ AtletikaiVerseny/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AtletikaiVerseny
+{
+    class Program
+    {
+        static List<Atleta> Atletak = new List<Atleta>();
+
+
+        static void Beolvas()
+        {
+            StreamReader sr = new StreamReader("tavol.csv");
+
+            while (!sr.EndOfStream)
+            {
+                Atletak.Add(new Atleta(sr.ReadLine()));
+            }
+        }
+
+        static void Masodik()
+        {
+            foreach (var a in Atletak)
+            {
+                Console.WriteLine($"2. feladat: {a.VezNev}  {a.KerNev} - {a.Ugras}");
+            }
+        }
+
+
+        static void Harmadik()
+        {
+            List<string> egyesuletek = new List<string>();
+
+            foreach (var a in Atletak)
+            {
+                if (!egyesuletek.Contains(a.Egyesulet))
+                {
+                    egyesuletek.Add(a.Egyesulet);
+                }
+            }
+        }
+
+        static void Negyedik()
+        {
+            int max = 0;
+
+            foreach (var a in Atletak)
+            {
+                if (a.Ugras > max)
+                {
+                    max = a.Ugras;
+
+                    Console.WriteLine($"4. feladat: {a.VezNev} {a.KerNev} - {max}");
+                }
+            }
+
+            
+        }
+
+        static void Otodik()
+        {
+            int atlag = 0;
+
+            foreach (var a in Atletak)
+            {
+                atlag += a.Ugras;
+            }
+            atlag = atlag / 9;
+        }
+
+        static void Hatodik()
+        {
+            StreamWriter sw = new StreamWriter("versenyzok.txt");
+            foreach (var a in Atletak)
+            {
+                sw.WriteLine();
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Beolvas();
+            Masodik();
+            Harmadik();
+            Negyedik();
+            Otodik();
+            Hatodik();
+
+            //foreach (var a in Atletak)
+            //{
+            //    Console.WriteLine($"{a.Rajtszam} - {a.VezNev} {a.KerNev} - {a.Egyesulet} - {a.Ugras}");
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
